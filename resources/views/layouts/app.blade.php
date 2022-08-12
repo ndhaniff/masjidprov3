7f0354803c2192c13f655ba12898257633f7bc14
Endy <endy@easywork.asia>
2022-08-12 08:40:46 +0800

    remove viewport

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> --}}
     <title>{{ config('app.name', 'Laravel') }}</title>
     @viteReactRefresh
     @vite('resources/js/app.js')
